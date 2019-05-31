9b95f170693045e904feefb1867767bb2c1203f5
sodastereo <f_er_@hotmail.com>
2019-05-31 00:13:28 -0400

    working app

---
+++ app/data/friends.js
@@ -0,0 +1,172 @@
+
+
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// But you could have it be an empty array as well.
+// ===============================================================================
+var friends = [
+    {
+        "name": "Sonya",
+        "photo": "http://www.stickpng.com/assets/thumbs/580b585b2edbce24c47b2dd3.png",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Alexis",
+        "photo": "https://3.bp.blogspot.com/-d2N8CJ6a0Hg/Wl50rZIe-XI/AAAAAAAAA6c/AHBVZXHGyb0kxnf_rFwXqz9yY3SMRZ9KACLcBGAs/s400/EBcw5pX0_400x400.jpeg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Angel",
+        "photo": "http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2a45.png",
+        "scores": [
+            9,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Karina",
+        "photo": "http://assets.stickpng.com/thumbs/58456b5bc42b384250a358d7.png",
+        "scores": [
+            5,
+            1,
+            4,
+            3,
+            2,
+            6,
+            9,
+            8,
+            9,
+            9
+        ]
+    },
+    {
+        "name": "Miriam",
+        "photo": "https://i.pinimg.com/736x/38/e7/f7/38e7f78b0203dbc8cc9b0b9511071225.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            3,
+            2,
+            6,
+            9,
+            8,
+            9,
+            9
+        ]
+    },
+    {
+        "name": "Lisa",
+        "photo": "https://pbs.twimg.com/profile_images/1106912876/IMG_0228_150px_400x400.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            3,
+            2,
+            6,
+            9,
+            8,
+            9,
+            9
+        ]
+    },
+    {
+        "name": "Nicole",
+        "photo": "https://www.praxisgertzen.de/wordpress/wp-content/uploads/2015/02/TeamMember_Berit400x400px.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            3,
+            2,
+            6,
+            9,
+            8,
+            9,
+            9
+        ]
+    },
+    {
+        "name": "Joanna",
+        "photo": "https://68.media.tumblr.com/4f65f7915fccada10bfc293775ec30a9/tumblr_okyzfc42K91unxg2jo8_400.png",
+        "scores": [
+            5,
+            1,
+            4,
+            3,
+            2,
+            6,
+            9,
+            8,
+            9,
+            9
+        ]
+    },
+    {
+        "name": "Niki",
+        "photo": "http://sodaarhitekti.hr/wp-content/uploads/Maja-Tomaško_400x400px-400x400.png",
+        "scores": [
+            5,
+            1,
+            4,
+            3,
+            2,
+            6,
+            9,
+            8,
+            9,
+            9
+        ]
+    },
+    {
+        "name": "Kendra",
+        "photo": "http://vignette2.wikia.nocookie.net/girlmeetsworld/images/5/50/J2wgXjLi_400x400.jpg/revision/latest?cb=20160310113835",
+        "scores": [
+            5,
+            1,
+            4,
+            3,
+            2,
+            6,
+            9,
+            8,
+            9,
+            9
+        ]
+    }
+]
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
